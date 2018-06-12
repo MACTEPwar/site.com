f420b5c552ca6f03ad0dc026ba405993ff570df6
Шебаниц Виталий Сергеевич <shebanits.vitaliy@med-service.dp.ua>
2018-06-12 19:54:33 +0300

    test1

--- test.php
+++ test.php
@@ -21,4 +21,23 @@
     //$s = 0;
     //var_dump($s);
     //echo "</pre>";
+    
 ?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./js/script.js"></script>
+</head>
+<body>
+    <div id="myId"><p>0</p></div>
+    <input type="button" id="clc">
+    <script>
+        $("#clc").click(function(){
+            $("#myId").text(Number($("#myId").text()) + 1);
+        });
+    </script>
+</body>
+</html>
